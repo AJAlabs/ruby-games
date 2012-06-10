455c9e4466814ef957738eb6cac5875383b1a6e4
AJ <aj@ajalabs.com>
2012-06-10 01:48:12 -0400

    another commit

---
+++ rubydice.rb
@@ -0,0 +1,27 @@
+# Random number generator simulating a dice game.
+
+
+# Choose how many dice to roll
+puts 'Dice Simulator.'
+puts "How many sides should the dice have?"
+sides = gets.chomp
+puts ''
+puts 'How many ' + sides + ' sided dice will you be rolling?'
+dice = gets.chomp
+puts 'When you are ready to roll ' + dice + ', ' + sides + ' sided dice, press Enter.'
+waiting = gets.chomp # Just a placehoder until I find a better way to wait for user input.
+puts ''
+dice_count = 1
+total = 0
+sides = sides.to_i
+dice = dice.to_i
+while dice_count <= dice
+  roll = 1 + rand(sides)
+  puts roll
+  dice_count += 1
+  total += roll.to_i
+end
+puts ''
+#Calculate the total sum of all dice
+puts 'Total Score: ' + total.to_s 
+puts ''
