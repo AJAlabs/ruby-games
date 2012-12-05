a677fb9368b88438f9f57ade8c911878e2206fd7
AJ <aj@ajalabs.com>
2012-12-05 01:54:52 -0500

    Added integer validation for sides and dice

--- rubydice.rb
+++ rubydice.rb
@@ -7,12 +7,32 @@
 
 
 # Choose how many dice to roll
-puts 'RubyDice'
+puts 'RubyDice - Let\'s roll some dice!'
 puts 'How many sides should the dice have?'
 sides = gets.chomp
+while sides.empty? do
+  puts 'No sides specified.'
+  puts 'How many sides should the dice have?'
+  sides = gets.chomp
+end
+while sides =~ /\D/ do
+  puts 'Invalid character'
+  puts 'Pleaes specify the number of sides on each dice (ie: 3)'
+  sides = gets.chomp
+end
 puts ''
 puts 'How many ' + sides + ' sided dice will you be rolling?'
 dice = gets.chomp
+while dice.empty? do
+  puts 'No dice specified.'
+  puts 'How many ' + sides + ' sided dice will you be rolling?'
+  dice = gets.chomp
+end
+while dice =~ /\D/ do
+  puts 'Invalid character'
+  puts 'Pleaes specify the number of dice to roll (ie: 3)'
+  dice = gets.chomp
+end
 puts 'When you are ready to roll ' + dice + ', ' + sides + ' sided dice, press Enter.'
 waiting = gets.chomp # Just a placehoder until I find a better way to wait for user input.
 puts ''
