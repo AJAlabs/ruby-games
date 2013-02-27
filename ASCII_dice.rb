3ed91e05de1ba2d3bfd5544ad9a1d772d35ea25e
AJ Acevedo <aj@ajalabs.com>
2013-02-27 02:37:17 -0500

    Added single puts ASCII dice

--- ASCII_dice.rb
+++ ASCII_dice.rb
@@ -1,37 +1,91 @@
 #!/usr/bin/env ruby 
 
+# Rolls a 1 on a D6 Dice
 puts %Q{...........}
 puts %Q{:         :}
 puts %Q{:    *    :}
 puts %Q{:         :}
 puts %Q{'''''''''''}
 
+# Rolls a 1 on a D6 dice
+puts "...........
+:         :
+:    *    :
+:         :
+'''''''''''
+"
+
+# Rolls a 2 on a D6 Dice
 puts %Q{...........}
 puts %Q{: *       :}
 puts %Q{:         :}
 puts %Q{:       * :}
 puts %Q{'''''''''''}
 
+# Rolls a 2 on a D6 dice
+puts "...........
+: *       :
+:         :
+:       * :
+'''''''''''
+"
+
+# Rolls a 3 on a D6 Dice
 puts %Q{...........}
 puts %Q{: *       :}
 puts %Q{:    *    :}
 puts %Q{:       * :}
 puts %Q{'''''''''''}
 
+# Rolls a 3 on a D6 dice
+puts "...........
+: *       :
+:    *    :
+:       * :
+''''''''''
+"
+
+# Rolls a 4 on a D6 Dice
 puts %Q{...........}
 puts %Q{: *     * :}
 puts %Q{:         :}
 puts %Q{: *     * :}
 puts %Q{'''''''''''}
 
+# Rolls a 4 on a D6 dice
+puts "...........
+: *     * :
+:         :
+: *     * :
+'''''''''''
+"
+
+# Rolls a 5 on a D6 Dice
 puts %Q{...........}
 puts %Q{: *     * :}
 puts %Q{:    *    :}
 puts %Q{: *     * :}
 puts %Q{'''''''''''}
 
+# Rolls a 5 on a D6 dice
+puts "...........
+: *     * :
+:    *    :
+: *     * :
+'''''''''''
+"
+
+# Rolls a 6 on a D6 Dice
 puts %Q{...........}
 puts %Q{: *     * :}
 puts %Q{: *     * :}
 puts %Q{: *     * :}
 puts %Q{'''''''''''}
+
+# Rolls a 6 on a D6 dice
+puts "...........
+: *     * :
+: *     * :
+: *     * :
+'''''''''''
+"
