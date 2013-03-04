8bbc3f224e2fe7106f5be44e6f6691dc2d805c77
AJ Acevedo <aj@ajalabs.com>
2013-03-04 01:56:57 -0500

    Added validation for two edge cases
    Edge Case 1. When an invalid character was entered, the form would except no input from the user. Edge Case 2. When no input was received from the user an invalid character was excepted.
    
    	modified:   rubydice.rb

--- rubydice.rb
+++ rubydice.rb
@@ -3,49 +3,88 @@
 # Copyright (c) 2013 AJ Acevedo | http://ajacevedo.com
 # This content is released under the MIT License.
 # http://www.opensource.org/licenses/mit-license.php
-# Version: 0.2.1
+# Version: 0.2.2
+
+# Load the library files
+$LOAD_PATH << './lib'
+require 'colorize.rb'
 
 
-# Choose how many dice to roll
+# Receive input for number of sides to the dice
 puts `clear`
 puts 'RubyDice - Let\'s roll some dice!'
-puts 'How many sides should the dice have?'
-sides = gets.chomp
-while sides.empty? do
-  puts 'No sides specified.'
-  puts 'How many sides should the dice have?'
-  sides = gets.chomp
-end
-while sides =~ /\D/ do
-  puts 'Invalid character'
-  puts 'Please specify the number of sides on each dice (ie: 3)'
-  sides = gets.chomp
-end
 puts ''
-puts 'How many ' + sides + ' sided dice will you be rolling?'
-dice = gets.chomp
-while dice.empty? do
-  puts 'No dice specified.'
-  puts 'How many ' + sides + ' sided dice will you be rolling?'
-  dice = gets.chomp
-end
-while dice =~ /\D/ do
-  puts 'Invalid character'
-  puts 'Please specify the number of dice to roll (ie: 3)'
-  dice = gets.chomp
+puts 'How many sides should the dice have?'.green
+@sides = nil
+@sides = gets.chomp
+
+# Begins the sides validation
+def sides_empty_validate
+  while @sides.empty? do
+    puts 'No sides specified.'.red
+    puts 'How many sides should the dice have?'.green
+    @sides = nil
+    @sides = gets.chomp
+    sides_invalid_validate
+  end
 end
 
-puts ''
-puts "When you are ready to roll #{dice}, #{sides} sided dice, press Enter."
-waiting = gets.chomp # Just a placeholder until I find a better way to wait for user input.
+def sides_invalid_validate
+  while @sides =~ /\D/ do
+    puts 'Invalid character'.red
+    puts 'Please specify the number of sides on each dice (ie: 3)'.green
+    @sides = nil
+    @sides = gets.chomp
+    sides_empty_validate
+  end
+end
 
-puts 'Your roll:'
+sides_empty_validate
+sides_invalid_validate
+
+# Receive input for the number of dice to roll
+puts ''
+puts "How many #{@sides} sided dice will you be rolling?".green
+@dice = nil
+@dice = gets.chomp
+
+# Begins the dice validation
+def dice_empty_validate
+  while @dice.empty? do
+    puts 'No dice specified.'.red
+    puts "How many #{@sides} sided dice will you be rolling?".green
+    @dice = nil
+    @dice = gets.chomp
+    dice_invalid_validate
+  end
+end
+
+def dice_invalid_validate
+  while @dice =~ /\D/ do
+    puts 'Invalid character'.red
+    puts 'Please specify the number of dice to roll (ie: 3)'.green
+    @dice = nil
+    @dice = gets.chomp
+    dice_empty_validate
+  end
+end
+
+dice_empty_validate
+dice_invalid_validate
+
+puts ''
+puts "When you are ready to roll #{@dice}, #{@sides} sided dice, press any key:"
+waiting = gets.chomp
+
+# Here is the magic behind the dice generator
+puts 'Your Roll'.underline + ':'
 dice_count = 1
 total = 0
-sides = sides.to_i
-dice = dice.to_i
-while dice_count <= dice
-  roll = 1 + rand(sides)
+@sides = @sides.to_i
+@dice = @dice.to_i
+
+while dice_count <= @dice
+  roll = 1 + rand(@sides)
   puts roll
   dice_count += 1
   total += roll.to_i
@@ -53,5 +92,5 @@ end
 
 puts ''
 # Calculate the total sum of all dice
-puts "Total Score: #{total}"
+puts 'Total Score'.underline + ': ' + "#{total}"
 puts ''
